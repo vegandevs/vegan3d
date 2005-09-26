fc18cc7e72c77ecd927982109085d01dd2becb71
Jari Oksanen <jari.oksanen@oulu.fi>
2005-09-26 00:00:00 +0000

    version 1.6-10

--- R/orgltext.R
+++ R/orgltext.R
@@ -1,10 +1,13 @@
 "orgltext" <-
-    function (object, text, display = "sites", choices = 1:3, justify = "center", ...) 
+    function (object, text, display = "sites", choices = 1:3, justify = "center",  
+              adj = 0.5, ...) 
 {
-    x <- scores(object, display = display, choices = choices, ...)
-    if (missing(text))
+    oldpak <- compareVersion(packageDescription("rgl", field="Version"), "0.65") == -1
+    x <- scores(object, display = display, choices = choices, 
+                ...)
+    if (missing(text)) 
         text <- rownames(x)
-    rgl.texts(x[,1],x[,2],x[,3], text, justify = justify, ...)
+    rgl.texts(x[, 1], x[, 2], x[, 3], text,
+              if (oldpak) justify = justify else adj = adj,  ...)
     invisible()
 }
-
