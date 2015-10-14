d5f221046308ec74a4e36c79aa60e283fde92721
Jari Oksanen <jari.oksanen@oulu.fi>
2015-10-14 17:46:13 +0300

    use weighted covariances in orglellipse

--- R/orglellipse.R
+++ R/orglellipse.R
@@ -1,21 +1,24 @@
 `orglellipse` <-
-    function(object, groups, display = "sites", kind = c("sd", "se"),
-             conf, choices = 1:3, alpha = 0.3, col = 2, ...)
+    function(object, groups, display = "sites", w = weights(object, display),
+             kind = c("sd", "se"), conf, choices = 1:3, alpha = 0.3,
+             col = 2, ...)
 {
+    weights.default <- function(object, ...) NULL
+    kind <- match.arg(kind)
     x <- scores(object, display = display, choices = choices, ...)
     groups <- as.factor(groups)
+    ## evaluate weights
+    w <- eval(w)
+    if (is.null(w) || length(w) == 1)
+        w <- rep(1, nrow(x))
     ## covariance and centres as lists
     Cov <- list()
     for (g in levels(groups))
-        Cov[[g]] <- cov(x[groups == g,, drop = FALSE])
-    Cnt <- list()
-    for (g in levels(groups))
-        Cnt[[g]] <- colMeans(x[groups == g,, drop = FALSE])
-    ## counts in groups
-    count <- table(groups)
+        Cov[[g]] <- cov.wt(x[groups == g,, drop = FALSE],
+                           wt = w[groups == g])
     if (kind == "se")
-        for(i in seq_along(count))
-            Cov[[i]] <- Cov[[i]]/count[i]
+        for(i in seq_len(length(Cov)))
+            Cov[[i]]$cov <- Cov[[i]]$cov/Cov[[i]]$n.obs
     ## recycle colours
     col <- rep(col, length = length(Cov))
     ## rgl::ellipse3d defaults to confidence envelopes, but we want to
@@ -27,6 +30,6 @@
         t <- sqrt(qchisq(conf, 3))
     ## graph
     for(i in seq_len(length(Cov)))
-        plot3d(ellipse3d(Cov[[i]], centre = Cnt[[i]], t = t),
+        plot3d(ellipse3d(Cov[[i]]$cov, centre = Cov[[i]]$center, t = t),
                add = TRUE, alpha = alpha, col = col[i], ...)
 }
