f34012d4f3a5d874395f1069205a1435a2b161eb
Jari Oksanen <jari.oksanen@oulu.fi>
2015-10-14 10:47:00 +0300

    add some arguments to configure rgl ordination ellipses

--- R/orglellipse.R
+++ R/orglellipse.R
@@ -1,16 +1,25 @@
 `orglellipse` <-
-    function(object, groups, display = "sites", choices = 1:3, ...)
+    function(object, groups, display = "sites", kind = c("sd", "se"),
+             conf = 0.95, choices = 1:3, alpha = 0.3, col = 2, ...)
 {
     x <- scores(object, display = display, choices = choices, ...)
     groups <- as.factor(groups)
+    ## covariance and centres as lists
     Cov <- list()
     for (g in levels(groups))
         Cov[[g]] <- cov(x[groups == g,, drop = FALSE])
     Cnt <- list()
     for (g in levels(groups))
         Cnt[[g]] <- colMeans(x[groups == g,, drop = FALSE])
+    ## counts in groups
     count <- table(groups)
+    if (kind == "se")
+        for(i in seq_along(count))
+            Cov[[i]] <- Cov[[i]]/count[i]
+    ## recycle colours
+    col <- rep(col, length = length(Cov))
+    ## graph
     for(i in seq_len(length(Cov)))
-        plot3d(ellipse3d(Cov[[i]], centre = Cnt[[i]]),
-               add = TRUE, alpha = 0.3, col = i)
+        plot3d(ellipse3d(Cov[[i]], centre = Cnt[[i]], level = conf, ...),
+               add = TRUE, alpha = alpha, col = col[i], ...)
 }
