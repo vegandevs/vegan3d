6365f9fe0e512879b0d1c20d047b0cf7b54caeab
Jari Oksanen <jari.oksanen@oulu.fi>
2015-10-14 17:18:07 +0300

    default to simple se/sd ellipses instead of confidence ellipses

--- R/orglellipse.R
+++ R/orglellipse.R
@@ -1,6 +1,6 @@
 `orglellipse` <-
     function(object, groups, display = "sites", kind = c("sd", "se"),
-             conf = 0.95, choices = 1:3, alpha = 0.3, col = 2, ...)
+             conf, choices = 1:3, alpha = 0.3, col = 2, ...)
 {
     x <- scores(object, display = display, choices = choices, ...)
     groups <- as.factor(groups)
@@ -18,8 +18,15 @@
             Cov[[i]] <- Cov[[i]]/count[i]
     ## recycle colours
     col <- rep(col, length = length(Cov))
+    ## rgl::ellipse3d defaults to confidence envelopes, but we want to
+    ## default to sd/se and only use confidence ellipses if conf is
+    ## given
+    if (missing(conf))
+        t <- 1
+    else
+        t <- sqrt(qchisq(conf, 3))
     ## graph
     for(i in seq_len(length(Cov)))
-        plot3d(ellipse3d(Cov[[i]], centre = Cnt[[i]], level = conf, ...),
+        plot3d(ellipse3d(Cov[[i]], centre = Cnt[[i]], t = t),
                add = TRUE, alpha = alpha, col = col[i], ...)
 }
