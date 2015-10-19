43556889f80b76ecc9721eeae0fc8065447c42e7
Jari Oksanen <jari.oksanen@oulu.fi>
2015-10-19 20:10:50 +0300

    Use mixture of point colours if a vector of colours was given

--- R/orglspantree.R
+++ R/orglspantree.R
@@ -7,10 +7,16 @@
     x <- scores(object, display = display, choices = choices, ...)
     ## get kids
     k <- spantree$kid
+    ## change colors to rgb in 0..1 and recycle if needed
+    col <- rep(col, length = nrow(x))
+    if (is.factor(col))
+        col <- as.numeric(col)
+    col <- col2rgb(col)/255
     ## lines
     for (i in 1:length(k)) {
         one <- x[i+1,]
         two <- x[k[i],]
-        rgl.lines(rbind(one, two), col = col, ...)
+        lcol <- rgb(t(col[, i+1] + col[,k[i]])/2)
+        rgl.lines(rbind(one, two), col = lcol, ...)
     }
 }
