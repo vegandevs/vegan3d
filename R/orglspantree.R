fd9fef4aa3685638613384a16f6af370b54ea462
Jari Oksanen <jari.oksanen@oulu.fi>
2015-10-20 11:54:35 +0300

    add orglcluster: an ordirgl equivalent of ordicluster
    This adds ordicluster-like web to the 3D orirgl plot. Earlier
    we had a function with the same name to draw a dendrogram tree over
    an (ordination) plane, but that was renamed to ordirgltree.

--- R/orglspantree.R
+++ R/orglspantree.R
@@ -1,3 +1,4 @@
+### Add lines from vegan::spantree object to an ordirgl plot
 `orglspantree` <-
     function(object, spantree, display = "sites", choices = 1:3, col = 1,
              ...)
@@ -18,5 +19,37 @@
         two <- x[k[i],]
         lcol <- rgb(t(col[, i+1] + col[,k[i]])/2)
         rgl.lines(rbind(one, two), col = lcol, ...)
+    }
+}
+
+### Add lines from an hclust object to an ordirgl plot
+
+`orglcluster` <-
+    function(object, cluster, prune = 0, display = "sites",
+             choices = 1:3, col = 1, ...)
+{
+    if (!inherits(cluster, "hclust"))
+        cluster <- as.hclust(cluster)
+    x <- scores(object, display = "sites", choices = choices, ...)
+    ## recycle colours and split to rgb 0..1
+    col <- rep(col, length = nrow(x))
+    ## (Ab)use vegan:::reorder.hclust to get the coordinates and
+    ## colours of internal nodes
+    node <- apply(x, 2, function(val) reorder(cluster, val)$value)
+    nodecol <- apply(col2rgb(col)/255, 1,
+                     function(val) reorder(cluster, val)$value)
+    nodecol <- rgb(nodecol)
+    ## go through merge matrix
+    merge <- cluster$merge
+    for(i in seq_len(nrow(merge)) - prune) {
+        if(merge[i,1] < 0)
+            one <- x[-merge[i,1],]
+        else
+            one <- node[merge[i,1],]
+        if (merge[i,2] < 0)
+            two <- x[-merge[i,2],]
+        else
+            two <- node[merge[i,2],]
+        rgl.lines(rbind(one, two), col = nodecol[i], ...)
     }
 }
