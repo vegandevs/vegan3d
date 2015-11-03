818a8b2161272bef862c6ed0dc5955fbae7ec579
Jari Oksanen <jari.oksanen@oulu.fi>
2015-11-03 11:15:39 +0200

    allow suppressing envfit display in constrained ordination

--- R/ordirgl.R
+++ R/ordirgl.R
@@ -46,6 +46,8 @@
         (is.list(object) && !is.null(object$CCA) && object$CCA$rank > 0)) {
         if (!missing(envfit)) 
             object <- envfit
+        if (is.na(envfit))
+            return()
         bp <- scores(object, dis = "bp", choices = choices)
         cn <- scores(object, dis = "cn", choices = choices)
         if (!is.null(cn) && !any(is.na(cn))) {
